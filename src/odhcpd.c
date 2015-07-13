69c2f4c4d46437299c9a61a51393771436b89015
Steven Barth <steven@midlink.org>
2015-07-13 23:01:19 +0200

    odhcpd: remove old workaround

--- src/odhcpd.c
+++ src/odhcpd.c
@@ -174,12 +174,6 @@ ssize_t odhcpd_send(int socket, struct sockaddr_in6 *dest,
 			|| IN6_IS_ADDR_MC_LINKLOCAL(&dest->sin6_addr))
 		dest->sin6_scope_id = iface->ifindex;
 
-	// IPV6_PKTINFO doesn't really work for IPv6-raw sockets (bug?)
-	if (dest->sin6_port == 0) {
-		msg.msg_control = NULL;
-		msg.msg_controllen = 0;
-	}
-
 	char ipbuf[INET6_ADDRSTRLEN];
 	inet_ntop(AF_INET6, &dest->sin6_addr, ipbuf, sizeof(ipbuf));
 

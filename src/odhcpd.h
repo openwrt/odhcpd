13ed21d635999a2935a05f84c95f9374c3207502
David Härdeman <david@hardeman.nu>
2024-02-09 14:14:03 +0100

    odhcpd: add DNR (RFC 9463) support
    Discovery of Network-designated Resolvers (DNR) allows devices on the network
    to discover encrypted DNS resolvers, which has so far required either manual
    configuration or other approaches (like systemd-resolved's "opportunistic"
    mode).
    
    To enable DNR, a new uci parameter has been added, which needs to contain at
    the very least, the priority (1-65535, lower = higher priority) and the server
    hostname (Authentication Domain Name, ADN, to use the wording of RFC9463):
    
    config dhcp 'lan'
            …
            list dnr '100 foobar.example.com'
    
    Optionally (and preferably), a comma-separated list of IP addresses and
    SvcParams can also be specified, like this (line wrapping added):
    
    config dhcp 'lan'
            …
            list dnr '100 resolver1.example.com
                        fda7:ab54:69fb::1,fda7:ab54:69fb::2,10.0.0.1
                        alpn=dot port=853'
            list dnr '200 resolver2.example.com
                        fda7:ab54:69fb::2,10.0.1.1,10.0.1.2
                        alpn=dot port=853'
    
    Client support is on it's way (e.g. in systemd PR #30952 or in the Windows
    Insiders program).
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- src/odhcpd.h
+++ src/odhcpd.h
@@ -37,6 +37,9 @@
 // RFC 8781 defines PREF64 option
 #define ND_OPT_PREF64 38
 
+// RFC 9463 - Discovery of Network-designated Resolvers (DNR)
+#define ND_OPT_DNR 144
+
 #define INFINITE_VALID(x) ((x) == 0)
 
 #define _unused __attribute__((unused))
@@ -242,6 +245,23 @@ struct dhcp_assignment {
 };
 
 
+// DNR - RFC9463
+struct dnr_options {
+	uint16_t priority;
+
+	uint8_t *adn;
+	uint16_t adn_len;
+
+	struct in_addr *addr4;
+	size_t addr4_cnt;
+	struct in6_addr *addr6;
+	size_t addr6_cnt;
+
+	uint8_t *svc;
+	uint16_t svc_len;
+};
+
+
 struct interface {
 	struct avl_node avl;
 
@@ -370,6 +390,10 @@ struct interface {
 	// SNTP
 	struct in6_addr *dhcpv6_sntp;
 	size_t dhcpv6_sntp_cnt;
+
+	// DNR
+	struct dnr_options *dnr;
+	size_t dnr_cnt;
 };
 
 extern struct avl_tree interfaces;

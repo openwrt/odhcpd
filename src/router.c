6ece28ffd475578b629d4b54173b0bcd423e707a
Paul Donald <newtwen+github@gmail.com>
2025-10-03 16:33:57 +0200

    config: do MaxRtrAdvInterval init at (ra_maxinterval) init time
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>
    Link: https://github.com/openwrt/odhcpd/pull/225
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- src/router.c
+++ src/router.c
@@ -345,16 +345,6 @@ static int calc_adv_interval(struct interface *iface, uint32_t lowest_found_life
 	if (*maxival > lowest_found_lifetime)
 		*maxival = lowest_found_lifetime;
 
-	if (*maxival > MaxRtrAdvInterval)
-		*maxival = MaxRtrAdvInterval;
-	else if (*maxival < 4)
-		*maxival = 4;
-
-	if (minival < MinRtrAdvInterval)
-		minival = MinRtrAdvInterval;
-	else if (minival > (*maxival * 3)/4)
-		minival = (*maxival >= 9 ? *maxival/3 : *maxival);
-
 	odhcpd_urandom(&msecs, sizeof(msecs));
 	msecs = (labs(msecs) % ((*maxival != minival) ? (*maxival - minival)*1000 : 500)) +
 			minival*1000;

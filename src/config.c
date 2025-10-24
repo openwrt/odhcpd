869e2231bbc93b5f3b7425d7214392b167d8b962
David Härdeman <david@hardeman.nu>
2025-10-25 00:19:09 +0200

    config: drop iface_attr_info
    BLOBMSG_TYPE_STRING is the default type for BLOBMSG_TYPE_ARRAY, so this
    doesn't add anything.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>
    Link: https://github.com/openwrt/odhcpd/pull/294
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- src/config.c
+++ src/config.c
@@ -194,16 +194,9 @@ static const struct blobmsg_policy iface_attrs[IFACE_ATTR_MAX] = {
 	[IFACE_ATTR_NTP] = { .name = "ntp", .type = BLOBMSG_TYPE_ARRAY },
 };
 
-static const struct uci_blob_param_info iface_attr_info[IFACE_ATTR_MAX] = {
-	[IFACE_ATTR_UPSTREAM] = { .type = BLOBMSG_TYPE_STRING },
-	[IFACE_ATTR_DNS] = { .type = BLOBMSG_TYPE_STRING },
-	[IFACE_ATTR_DOMAIN] = { .type = BLOBMSG_TYPE_STRING },
-};
-
 const struct uci_blob_param_list interface_attr_list = {
 	.n_params = IFACE_ATTR_MAX,
 	.params = iface_attrs,
-	.info = iface_attr_info,
 };
 
 const struct blobmsg_policy lease_cfg_attrs[LEASE_CFG_ATTR_MAX] = {

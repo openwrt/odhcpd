openwrt/odhcpd

commit b9db4d7061a08bf82a25222074065cce71973d0c

dhcpv6: handle realloc failure
Another realloc failure case.

Signed-off-by: David Härdeman <david@hardeman.nu>
Link: https://github.com/openwrt/odhcpd/pull/320
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

src/dhcpv6-ia.c
@@ -1236,8 +1236,9 @@ proceed:
 				    hdr->msg_type == DHCPV6_MSG_REQUEST ||
 				    hdr->msg_type == DHCPV6_MSG_REBIND)) {
 				if ((!(a->flags & OAF_STATIC) || !a->hostname) && hostname_len > 0) {
-					a->hostname = realloc(a->hostname, hostname_len + 1);
-					if (a->hostname) {
+					char *hostname = realloc(a->hostname, hostname_len + 1);
+					if (hostname) {
+						a->hostname = hostname;
 						memcpy(a->hostname, hostname, hostname_len);
 						a->hostname[hostname_len] = 0;
 

commit d21e504b38ab4c880c43b7f1649104bb2f0d2d8b

dhcpv6-ia: fix realloc bug
Commit b9db4d7061a08bf82a25222074065cce71973d0c introduced a bug, the
"hostname" variable used for the realloc would shadow the real hostname
defined at the beginning of the function. Fix this by using a different
variable name.

Fixes: b9db4d7061a0 ("dhcpv6: handle realloc failure")
Signed-off-by: David Härdeman <david@hardeman.nu>
Link: https://github.com/openwrt/odhcpd/pull/326
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

src/dhcpv6-ia.c
@@ -1236,9 +1236,9 @@ proceed:
 				    hdr->msg_type == DHCPV6_MSG_REQUEST ||
 				    hdr->msg_type == DHCPV6_MSG_REBIND)) {
 				if ((!(a->flags & OAF_STATIC) || !a->hostname) && hostname_len > 0) {
-					char *hostname = realloc(a->hostname, hostname_len + 1);
-					if (hostname) {
-						a->hostname = hostname;
+					char *tmp = realloc(a->hostname, hostname_len + 1);
+					if (tmp) {
+						a->hostname = tmp;
 						memcpy(a->hostname, hostname, hostname_len);
 						a->hostname[hostname_len] = 0;
 
c23d13147c18b20f57b53c9e380f0171224e6b66
David Härdeman <david@hardeman.nu>
2025-11-28 11:48:59 +0100

    config: move pio json handling to statefiles.c
    The prefix information files are also examples of statefiles, and there
    are some things which can be shared between e.g. the state file logic
    and the pio file logic (like the conversion from MONOTIME to time_t and
    back, and also the use of tmp files). That's for later patches though,
    this just moves the code without any code changes to make subsequent
    patches clearer.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- src/statefiles.h
+++ src/statefiles.h
@@ -1,5 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2024 David Härdeman <david@hardeman.nu>
+ * SPDX-FileCopyrightText: 2025 Álvaro Fernández Rojas <noltari@gmail.com>
  *
  * SPDX-License-Identifier: GPL2.0-only
  */
@@ -9,6 +10,10 @@
 
 #define ODHCPD_HOSTS_FILE_PREFIX "odhcpd.hosts"
 #define ODHCPD_TMP_FILE ".odhcpd.tmp"
+
+void config_load_ra_pio(struct interface *iface);
+
+void config_save_ra_pio(struct interface *iface);
 
 bool statefiles_write(void);
 

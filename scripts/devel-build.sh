openwrt/odhcpd

commit ef8de928da7f445649ab8e0aa8360166a04131f0

build: enable DHCPV4_SUPPORT and UBUS
Enable DHCPV4_SUPPORT and UBUS in order to correctly build all the source
code.

Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

scripts/devel-build.sh
@@ -94,6 +94,8 @@ cd "${ODHCPDDIR}"
 cmake							\
 	-S .						\
 	-B "${BUILDDIR}"				\
+	-DDHCPV4_SUPPORT=ON				\
+	-DUBUS=ON					\
 	-DCMAKE_PREFIX_PATH="${BUILDDIR}"		\
 	${BUILD_ARGS}
 make -C "${BUILDDIR}"

commit bb2da6dcb58d2359059b1aca126fcf8c828f32f4

build: fix BUILD_ARGS
Commit ef8de928da7f forced DHCPV4_SUPPORT and UBUS cmake options, but should
have fixed BUILD_ARGS instead (-WITH_UBUS=ON -> -DUBUS=ON).

Fixes: ef8de928da7f ("build: enable DHCPV4_SUPPORT and UBUS")
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

scripts/devel-build.sh
@@ -11,7 +11,7 @@ if [ ! -e "CMakeLists.txt" ] || [ ! -e "src/odhcpd.c" ]; then
 fi
 
 if [ $# -eq 0 ]; then
-	BUILD_ARGS="-DDHCPV4_SUPPORT=ON -WITH_UBUS=ON"
+	BUILD_ARGS="-DDHCPV4_SUPPORT=ON -DUBUS=ON"
 else
 	BUILD_ARGS="$@"
 fi
@@ -94,8 +94,6 @@ cd "${ODHCPDDIR}"
 cmake							\
 	-S .						\
 	-B "${BUILDDIR}"				\
-	-DDHCPV4_SUPPORT=ON				\
-	-DUBUS=ON					\
 	-DCMAKE_PREFIX_PATH="${BUILDDIR}"		\
 	${BUILD_ARGS}
 make -C "${BUILDDIR}"
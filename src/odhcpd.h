5a173995caf1bf6d76f2473fe855e526e8feee1f
Dávid Benko <davidbenko@davidbenko.dev>
2026-04-20 22:10:54 +0200

    router: add option to include static routes in RA
    Introduces a new configuration section `ra_route` for adding custom
    route(s) in route advertisement (RA) messages.
    
    This is useful when, for example, end devices are given only ULA IPv6
    addresses (e.g. from `fc00:0:100::/48`), but one still needs inter-subnet
    communication to e.g. `fc00:0:200::/48`. Currently, this isn't possible
    without a static route on each end device, as the odhcpd server doesn't
    present itself as a default router and no route information is provided
    other than the on-link local ULA subnet (`fc00:0:100::/48`). Even when
    odhcpd provides a default route, route customization may be useful in
    a multi-homed setup to set higher or lower route preferences and enforce
    different routing behavior (see RFC4191).
    
    The new configuration section `ra_route` allows advertising arbitrary
    routes to end devices, enabling correct routing even when no default
    route (or public IPv6 prefix) is available.
    
    The configuration section includes four attributes:
    - prefix: the prefix itself, required
    - interface: optional list of interfaces to advertise this route on (all
      odhcpd interfaces are used by default)
    - preference: optional preference value for this route (low, medium, or
      high)
    - lifetime: optional lifetime override
    
    Config example:
    
    ```
    config ra_route 'route_branch1'
    	option prefix 'fc00:10::/32'
    	list interface 'lan'
    	list interface 'guest_wifi'
    	option preference 'high'
    	option lifetime '2h'
    
    config ra_route 'route_branch2'
    	option prefix 'fc00:20::/32'
    	option preference 'low'
    	option lifetime 'infinite'
    
    config ra_route 'route_branch3'
    	option prefix 'fc00:30::/32'
    ```
    
    This patch is rework of my PR: https://github.com/openwrt/odhcpd/pull/224
    The previous implementation used new attributes on both global and
    per-interface basis to configure the static routes. This meant that
    per-route preference and lifetime couldn't be set, and, most
    importantly, the original PR didn't take RFC4191 into account, allowing
    configuration of static routes only when a default route wasn't present.
    Paul Donald <newtwen+github@gmail.com> and I agreed on this redesign
    (see the discussion of the original PR).
    
    Fixes: https://github.com/openwrt/odhcpd/issues/74
    
    Signed-off-by: Dávid Benko <davidbenko@davidbenko.dev>

--- src/odhcpd.h
+++ src/odhcpd.h
@@ -181,6 +181,17 @@ struct odhcpd_ipaddr {
 	};
 };
 
+struct odhcpd_ip6prefix {
+	struct in6_addr addr;
+	uint8_t len;
+};
+
+struct odhcpd_ra_route {
+	struct odhcpd_ip6prefix prefix;
+	int8_t preference;
+	uint32_t lifetime;
+};
+
 enum odhcpd_mode {
 	MODE_DISABLED,
 	MODE_SERVER,
@@ -421,6 +432,8 @@ struct interface {
 	uint8_t pref64_length;
 	uint8_t pref64_plc;
 	uint32_t pref64_prefix[3];
+	struct odhcpd_ra_route *ra_static_routes;
+	size_t ra_static_routes_cnt;
 	bool no_dynamic_dhcp;
 	bool have_link_local;
 	uint8_t pio_filter_length;
